c22575406f855224dcbac8ceb907a51c590cf8cf
ramakrishna8254 <109083683+ramakrishna8254@users.noreply.github.com>
2023-02-17 19:07:30 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -12,8 +12,8 @@ sonarqubeScanner({
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
             'sonar.sources': '.',
-	    'sonar.exclusions': '**/node_modules/**,/coverage/lcov-report/*.test/*.js'
-	     'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info'  
+	    'sonar.exclusions': '**/node_modules/**,/coverage/lcov-report/*.test/*.js',
+	    'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
 	    //'sonar.tests': 'specs',
             //'sonar.inclusions' : 'src/**'
        },
