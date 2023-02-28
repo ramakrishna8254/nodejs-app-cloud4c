784b5d98dd5c36ca2e20a895f51bf18b7ddc7f90
ramakrishna8254 <109083683+ramakrishna8254@users.noreply.github.com>
2023-02-28 18:13:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('WELCOME TO Cloud4C Technologies Hyderabad.........AWS DEVOPS WORKING PARTNER.......A CTRLS Datacenter Company Pvt Ltd, Madhapur, Hyderabad, India'); //write a response to the client
+  res.write('WELCOME TO Cloud4C Technologies Hyderabad.........AWS DEVOPS WORKING PARTNER.......A CTRLS Datacenter Company Pvt Ltd, Madhapur, Hyderabad, India, Surneni Ramakrishna Devops Engineer Topper in the Batch Krishna manohar IPS'); //write a response to the client
   res.end(); //end the response
 }).listen(9981); //the server object listens on port 80
